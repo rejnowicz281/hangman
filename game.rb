1a96345e75d42a93668dc236d7d15956f2b27e4a
Alan <alanrejnowicz281@gmail.com>
2020-07-27 12:40:19 +0200

    Added ability to save and load

--- game.rb
+++ game.rb
@@ -1,3 +1,5 @@
+require "yaml"
+
 class Game 
   attr_reader :secret_word, :secret_word_array, :missed_letters, :guessed_letters, :try
 
@@ -5,7 +7,7 @@ class Game
     @secret_word = secret_word
     @secret_word_array = secret_word.split("")
     @missed_letters = []
-    @guessed_letters = @secret_word_array.map {|word| word = "?"}
+    @guessed_letters = secret_word.split("").map {|word| word = "?"}
     @try = 1
   end
 
@@ -19,7 +21,53 @@ class Game
       random_word
     end
   end
-  
+
+  def save?
+    save_or_not = ""
+    loop do 
+      print "Would you like to save the game?(Y/N): "; save_or_not = gets.chomp.upcase; puts 
+      break if save_or_not == "Y" || save_or_not == "N"
+    end
+
+    case save_or_not
+      when "Y"
+        save_slot = ""
+        loop do  
+          print "Which slot do you want do save on?(save1, save2, save3, save4, save5): "; save_slot = gets.chomp; puts 
+          break if save_slot == "save1" || save_slot == "save2" || save_slot == "save3" || save_slot == "save4" || save_slot == "save5"
+        end
+
+        slot = File.open("saves/#{save_slot}.yaml", "w")
+        slot.write to_yaml
+        slot.close
+      when "N"
+        return
+    end
+  end
+
+  def load?
+    load_or_not = ""
+    loop do 
+      print "Would you like to load a game?(Y/N): "; load_or_not = gets.chomp.upcase; puts 
+      break if load_or_not == "Y" || load_or_not == "N"
+    end
+
+    case load_or_not
+      when "Y"
+        save_slot = ""
+        loop do  
+          print "Which slot do you want to load?(save1, save2, save3, save4, save5): "; save_slot = gets.chomp; puts 
+          break if save_slot == "save1" || save_slot == "save2" || save_slot == "save3" || save_slot == "save4" || save_slot == "save5"
+        end
+
+        slot = File.read("saves/#{save_slot}.yaml")
+        game = YAML::load(slot)
+        game.play
+      when "N"
+        return
+    end
+  end
+
   def show_missed_letters
     puts "Missed letters: #{@missed_letters.join(", ")}"; puts 
   end
@@ -68,13 +116,16 @@ class Game
   end
 
   def play
-    puts @secret_word
+    puts "Welcome to hangman!"; puts
+    load?
 
     while @try < 9
       puts "-(Tries left: #{9 - @try})"; puts
       
       show_missed_letters
       show_guessed_letters
+
+      save?
   
       guess = get_guess
   
