2f52e2ae9218a7897fe16d0f4edb0a59762fa989
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2020-11-26 21:00:22 +0100

    Updated code, added new features and updated old ones

--- game.rb
+++ game.rb
@@ -1,144 +1,102 @@
-require "yaml"
-
-class Game 
-  attr_reader :secret_word, :secret_word_array, :missed_letters, :guessed_letters, :try
-
-  def initialize(secret_word)
-    @secret_word = secret_word
-    @secret_word_array = secret_word.split("")
-    @missed_letters = []
-    @guessed_letters = secret_word.split("").map {|word| word = "?"}
-    @try = 1
-  end
-
-  def Game.random_word 
-    words = File.read("5desk.txt")
-    random_word = words.split.sample.downcase 
-    
-    if random_word.length < 5 || random_word.length > 12
-      Game.random_word
-    else
-      random_word
-    end
-  end
-
-  def save?
-    save_or_not = ""
-    loop do 
-      print "Would you like to save the game?(Y/N): "; save_or_not = gets.chomp.upcase; puts 
-      break if save_or_not == "Y" || save_or_not == "N"
-    end
-
-    case save_or_not
-      when "Y"
-        save_slot = ""
-        loop do  
-          print "Which slot do you want do save on?(save1, save2, save3, save4, save5): "; save_slot = gets.chomp; puts 
-          break if save_slot == "save1" || save_slot == "save2" || save_slot == "save3" || save_slot == "save4" || save_slot == "save5"
-        end
-
-        slot = File.open("saves/#{save_slot}.yaml", "w")
-        slot.write to_yaml
-        slot.close
-      when "N"
-        return
-    end
-  end
-
-  def load?
-    load_or_not = ""
-    loop do 
-      print "Would you like to load a game?(Y/N): "; load_or_not = gets.chomp.upcase; puts 
-      break if load_or_not == "Y" || load_or_not == "N"
-    end
-
-    case load_or_not
-      when "Y"
-        save_slot = ""
-        loop do  
-          print "Which slot do you want to load?(save1, save2, save3, save4, save5): "; save_slot = gets.chomp; puts 
-          break if save_slot == "save1" || save_slot == "save2" || save_slot == "save3" || save_slot == "save4" || save_slot == "save5"
-        end
-
-        slot = File.read("saves/#{save_slot}.yaml")
-        game = YAML::load(slot)
-        game.play
-      when "N"
-        return
-    end
-  end
-
-  def show_missed_letters
-    puts "Missed letters: #{@missed_letters.join(", ")}"; puts 
-  end
-
-  def show_guessed_letters
-    puts "Guessed letters: #{@guessed_letters.join(" | ")}"; puts
-  end
-
-  def get_guess
-    guess = ""
-    loop do 
-      print "Type in a letter: "; guess = gets.chomp
-      redo if @missed_letters.include?(guess) || @guessed_letters.include?(guess)
-      break if (guess.length == 1 && guess.to_i == 0)
-    end
-    guess.downcase
-  end
-
-  def check_if_include(guess)
-    if @secret_word_array.include?(guess)
-      (@secret_word_array.length).times { |i| @guessed_letters[i] = guess if @secret_word_array[i] == guess } 
-    else
-      @missed_letters << guess
-      @try += 1
-    end
-  end
-
-  def won?
-    @guessed_letters == @secret_word_array
-  end
-
-  def win_message
-    puts
-    show_guessed_letters
-    puts "You won! Secret word: #{@secret_word}"
-  end
-
-  def lost?
-    @try == 9 && @guessed_letters != @secret_word_array
-  end
-
-  def lose_message
-    puts
-    show_guessed_letters
-    puts "You lost. Secret word: #{@secret_word}"
-  end
+class Hangman
+  attr_reader :saves
+  
+  def initialize(incorrect_guesses = 8, secret_word = random_word)
+      @secret_word = secret_word
+      @incorrect_guesses = incorrect_guesses
+      @correct_letters = Array.new(secret_word.length, "_")
+      @incorrect_letters = []
+  end 
 
   def play
-    puts "Welcome to hangman!"; puts
-    load?
+      @ask_to_save = true
+      puts "Welcome to Hangman!"
+      load?
 
-    while @try < 9
-      puts "-(Tries left: #{9 - @try})"; puts
-      
-      show_missed_letters
-      show_guessed_letters
+      loop do
+          if @incorrect_guesses == 0 
+              puts "You lose. Secret word: #{@secret_word}"
+              return
+          elsif @correct_letters.join("") == @secret_word
+              puts "You win. Secret word: #{@secret_word}"
+              return
+          end 
+          puts "-- #{@incorrect_guesses} incorrect guesses left --"
+          puts
+          puts "#{@correct_letters.join(" | ")}"
+          puts "\nIncorrect: #{@incorrect_letters.join(" | ")}" unless @incorrect_letters.empty?
+          puts
+          
+          save? if @ask_to_save
 
-      save?
-  
-      guess = get_guess
-  
-      check_if_include(guess)
-  
-      return win_message if won?
-      return lose_message if lost?
-  
-      puts
-    end
+          letter = get_letter
+          secret_array = @secret_word.split("")
+          
+          if secret_array.include?(letter)
+              secret_array.length.times { |i| @correct_letters[i] = letter if letter == secret_array[i]}
+          else
+              @incorrect_letters << letter 
+              @incorrect_guesses -= 1
+          end 
+          puts
+      end 
+  end 
+
+  private
+  def random_word
+      dictionary = File.read("5desk.txt").split
+      random_word = ""
+      random_word = dictionary.sample.downcase until random_word.length > 5 && random_word.length < 12
+      random_word
   end
+
+  def get_letter
+      print "Type in a letter: "; letter = gets.chomp
+      return (letter.length != 1 || letter.to_i != 0 || letter == "0" || @incorrect_letters.include?(letter) || @correct_letters.include?(letter)) ? get_letter : letter.downcase
+  end 
+
+  def load?
+      saves_array = Dir.children("saves")
+      return (puts "\nNo available saves.") if saves_array.empty?
+      print "Would you like to load a game?(yes, no): "; choice = gets.chomp
+      if choice == "yes"
+          puts "List of available saves: "
+          puts
+          saves_array.length.times do |i|
+              save = File.read("saves/save#{i}.marshal")
+              puts "Save #{i}), Time of save (#{Marshal::load(save).time_of_save})"
+          end 
+          print "Choose the save you want to load(type index): "; load_idx = gets.chomp
+          save = File.read("saves/save#{load_idx}.marshal")
+          new_game = Marshal::load(save)
+          return new_game.play
+      end 
+  end 
+
+  def save?
+      saves_array = Dir.children("saves")
+      print "Would you like to save the game?(yes, no, dont_ask): "; choice = gets.chomp
+      if choice == "yes"
+          @time_of_save = Time.now
+          save = File.new("saves/save#{saves_array.length}.marshal", "w")
+          save.write "#{Marshal::dump(self)}"
+          save.close
+          puts "\nGame saved on #{@time_of_save}"
+          puts
+      elsif choice == "dont_ask"
+        @ask_to_save = false
+      end 
+  end  
+
+  def time_of_save
+    @time_of_save
+  end 
+
+  def secret_word 
+    @secret_word
+  end 
 end
 
-game = Game.new(Game.random_word)
+game = Hangman.new
 
 game.play
